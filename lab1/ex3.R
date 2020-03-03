0829259d19a7224d20843296d2c56fe4650e008c
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-03-03 12:43:10 +0100

    Fixed bug in -  3 exercise

--- lab1/ex3.R
+++ lab1/ex3.R
@@ -16,3 +16,27 @@ head(kredyt, 10)
 tail(kredyt, 10)
 
 dim(kredyt)
+
+print("Wiersze od 100 do 125")
+print(kredyt[100:125,])
+
+print("Pierwsze 20 wierszow")
+print(head(kredyt, 20))
+
+print("Ostatnie 30 wierszów")
+print(tail(kredyt, 30))
+
+print("Wiersze od 20 do 30 i od 50 do 60")
+print(kredyt[c(20:30, 50:60),])
+
+print("Co dziesieta rate")
+print(kredyt[seq(10, to=dim(kredyt)[1], by=10),])
+
+ratyKapitaluSum <- sum(kredyt[,1])
+odsetkiSum      <- sum(kredyt[,2])
+ratyKredytuSum  <- sum(kredyt[,3])
+
+data.frame(ratyKapitaluSum, odsetkiSum, ratyKredytuSum)
+Sum <- ratyKapitaluSum + odsetkiSum + ratyKredytuSum
+
+when <- which(rataKapitalu > odsetki)[1]
