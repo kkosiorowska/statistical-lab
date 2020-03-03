3f6e30e2dbc6c1e28f65beeed8f15ed8cb2a7d1a
kkosiorowska <kaarolina.kosiorowska@gmail.com>
2020-03-03 11:21:57 +0100

    Adding 1 exercise

---
+++ lab1/ex1.R
@@ -0,0 +1,20 @@
+a <- c(1, 4, 6, 13, -10)
+aLength <- length(a)
+aMax <- max(a)
+aMin <- min(a)
+
+b<- c(seq(1, 101, by=2))
+bLength <- length(b)
+bMMax <- max(b)
+bMin <- min(b)
+
+c <- rep(c(4, 7, 9), each=4)
+cLength <- length(c)
+cMax <- max(c)
+cMin <- min(c)
+
+d <- rep(c(4, 7, 9), times=6)
+dLength <- length(d)
+dMax <- max(d)
+dMin <- min(d)
+
